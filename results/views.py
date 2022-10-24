957f0a1f6fd8ee738747e616e1875d7b6fef031d
goodieboy <oyedeleyusuff@gmail.com>
2022-10-24 15:53:56 +0100

    commented all print statement

--- results/views.py
+++ results/views.py
@@ -45,17 +45,17 @@ class AddStudentResultView(LoginRequiredMixin, View):
             student = form.cleaned_data.get('student')
             r_term = form.cleaned_data.get('r_term')
             r_session = form.cleaned_data.get('session')
-            print(r_term, type(r_term))
-            print(r_session, type(r_session))
+            # print(r_term, type(r_term))
+            # print(r_session, type(r_session))
             try:
                 term = Term.objects.get_or_create(session=r_session, term=r_term)
                 get_term = Term.objects.get(term=term[0])
-                print(get_term.id)
+                # print(get_term.id)
             except IntegrityError:
                 return redirect('results:add-result')
 
             if not Result.objects.filter(student=student, session=r_session, term=get_term).exists():
-                print(student)
+                # print(student)
                 for result in result_form.cleaned_data:
                     Result.objects.create(
                         student=student,
@@ -71,9 +71,9 @@ class AddStudentResultView(LoginRequiredMixin, View):
                 )
             messages.error(self.request, 'Make sure the student result is not exists before..')
             return redirect('results:add-result')
-        print(form.errors)
-        print(result_form.errors)
-        print(result_form.non_form_errors())
+        # print(form.errors)
+        # print(result_form.errors)
+        # print(result_form.non_form_errors())
         return redirect('results:add-result')
 
 
@@ -83,11 +83,11 @@ class ResultView(LoginRequiredMixin, View):
         # students = []
         if request.user.is_superuser:
             results = Result.objects.all().values_list('student', flat=True)
-            print(results)
+            # print(results)
             students = Student.objects.filter(id__in=results).exclude(is_suspended=True)
         elif ClassAdmin.objects.filter(id=request.user.id).exists():
             results = Result.objects.filter(student__classroom__teacher=request.user).values_list('student', flat=True)
-            print(results)
+            # print(results)
             students = Student.objects.filter(id__in=results).exclude(is_suspended=True)
         else:
             messages.info(self.request, "You need to obtain token to access this page.")
@@ -131,8 +131,8 @@ class CheckResultView(LoginRequiredMixin, View):
 
         if form.is_valid():
             user_token = form.cleaned_data.get('token')
-            print(user_token)
-            print(type(user_token))
+            # print(user_token)
+            # print(type(user_token))
             try:
                 student_id = Token.objects.get(token=user_token)
                 # print(student_id)
@@ -165,15 +165,15 @@ class UploadResultView(LoginRequiredMixin, View):
 
     def post(self, request, *args, **kwargs):
         result_raw_file = request.FILES.get('result_file')
-        print(result_raw_file, type(result_raw_file))
+        # print(result_raw_file, type(result_raw_file))
         if result_raw_file:
             result_file = result_raw_file.read().decode('utf-8')
-            print(result_file)
+            # print(result_file)
         else:
             return redirect('results:result')
         current_student_id = {}
         for data in csv.DictReader(io.StringIO(result_file)):
-            print(data)
+            # print(data)
             student = Student.objects.filter(student_id=data['student_id']).first()
             session = Session.objects.filter(name_of_session=data['session']).first()
             subject = Subject.objects.filter(name__icontains=data['subject']).first()
